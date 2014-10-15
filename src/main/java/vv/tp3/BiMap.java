1cfd6ee886f64ae0cf33dc19fa35c333ba680f44
josian <marinier.josian@yahoo.fr>
2014-10-15 11:53:11 +0200

    implementation BiMap

--- src/main/java/vv/tp3/BiMap.java
+++ src/main/java/vv/tp3/BiMap.java
@@ -5,11 +5,46 @@ package vv.tp3;
  */
 public class BiMap<K,V> extends SimpleMap<K,V> {
 
+
     public K getByValue(Object value) {
-        return null;
+
+        int index =valueList.indexOf(value);
+        if(index!= -1)
+        {
+            return keyList.get(index);
+        }
+        else
+        {
+            return null;
+        }
     }
 
     public K removeValue(Object key) {
+
+
         return null;
     }//
+
+    @Override
+    public V put(K key, V value)
+    {
+        int index =valueList.indexOf(value);
+        if(index != -1)
+        {
+            keyList.set(index, key);
+        }
+        else
+        {
+            int indexk =keyList.indexOf(value);
+            if(indexk != -1)
+            {
+                valueList.set(indexk, value);
+            }
+            else
+            {
+                super.put(key, value);
+            }
+        }
+        return value;
+    }
 }
