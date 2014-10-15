4858ad39b3578f2cf27434e1217f98fbeb9cb71c
josian <marinier.josian@yahoo.fr>
2014-10-15 11:03:47 +0200

    implementation SimpleMap

--- src/main/java/vv/tp3/SimpleMap.java
+++ src/main/java/vv/tp3/SimpleMap.java
@@ -3,65 +3,99 @@ package vv.tp3;
 import java.util.*;
 
 public class SimpleMap<K,V> implements Map<K,V> {
-//
+
+    List<K> keyList = new ArrayList();
+    List<V> valueList = new ArrayList();
 
     @Override
     public int size() {
-        return 0;
+        return keyList.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return keyList.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        return keyList.contains(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        return valueList.contains(value);
     }
 
     @Override
     public V get(Object key) {
-        return null;
+        return valueList.get(keyList.indexOf(key));
     }
 
     @Override
     public V put(K key, V value) {
-        return null;
+        keyList.add(key);
+        valueList.add(value);
+        return value;
     }
 
     @Override
     public V remove(Object key) {
-        return null;
+        int index = keyList.indexOf(key);
+        if(index==-1) //si le key n'existe pas
+        {
+            return null;
+        }
+        else
+        {
+            keyList.remove(index);
+            V elt = valueList.get(index);
+            valueList.remove(index);
+            return elt;
+        }
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
 
+
+        Iterator iterator = m.keySet().iterator();
+        while(iterator.hasNext()){
+            Object key   = iterator.next();
+            Object value = m.get(key);
+            keyList.add(((K) key));
+            valueList.add(((V) value));
+        }
     }
 
     @Override
     public void clear() {
-
+        keyList.clear();
+        valueList.clear();
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+
+        return new HashSet(keyList);
     }
 
     @Override
     public Collection<V> values() {
-        return null;
+        return valueList;
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        Set s = new HashSet();
+        Iterator iterator = keyList.iterator();
+        while(iterator.hasNext())
+        {
+            K key   =(K) iterator.next();
+            V value =get(key);
+            Entry<K,V> e = new AbstractMap.SimpleEntry<K, V>(key, value);
+            s.add(e);
+        }
+        return s;
     }
 }
